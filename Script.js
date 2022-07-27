e1b293ac929ebc6887435e1c15becbd5070c03d2
Zainab Oladipupo <69272842+oladipupozainab@users.noreply.github.com>
2022-07-27 22:30:34 +0100

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,84 @@
+// JavaScript source code
+const change = document.getElementById("image1");
+
+change.addEventListener("click", (event) => {
+    event.target.style.background = "red"
+    event.target.style.color = "white"
+});
+document.addEventListener("click", function () {
+    document.getElementById("image1").innerHTML = "Tea";
+});
+const change2 = document.getElementById("image2");
+
+change2.addEventListener("click", (event) => {
+    event.target.style.background = "pink"
+    event.target.style.color = "blue"
+});
+document.addEventListener("click", function () {
+    document.getElementById("image2").innerHTML = "Food";
+});
+
+const change3 = document.getElementById("image3");
+
+change3.addEventListener("click", (event) => {
+    event.target.style.background = "blue"
+    event.target.style.color = "white"
+});
+document.addEventListener("click", function () {
+    document.getElementById("image3").innerHTML = "Fruit";
+});
+
+const change4 = document.getElementById("image4");
+
+change4.addEventListener("click", (event) => {
+    event.target.style.background = "black"
+    event.target.style.color = "white"
+});
+document.addEventListener("click", function () {
+    document.getElementById("image4").innerHTML = "Flower";
+});
+
+const change5 = document.getElementById("image5");
+
+change5.addEventListener("click", (event) => {
+    event.target.style.background = "grey"
+    event.target.style.color = "blue"
+});
+document.addEventListener("click", function () {
+    document.getElementById("image5").innerHTML = "Food";
+});
+
+const change6 = document.getElementById("image6");
+
+change6.addEventListener("click", (event) => {
+    event.target.style.background = "yellow"
+    event.target.style.color = "black"
+});
+document.addEventListener("click", function () {
+    document.getElementById("image6").innerHTML = "Fruit";
+});
+
+const change7 = document.getElementById("image7");
+
+change7.addEventListener("click", (event) => {
+    event.target.style.background = "gold"
+    event.target.style.color = "blue"
+});
+document.addEventListener("click", function () {
+    document.getElementById("image7").innerHTML = "Pancake";
+});
+
+const change8 = document.getElementById("image8");
+
+change8.addEventListener("click", (event) => {
+    event.target.style.background = "purple"
+    event.target.style.color = "white"
+});
+document.addEventListener("click", function () {
+    document.getElementById("image8").innerHTML = "Orange";
+});
+
+
+  
+
+                       
